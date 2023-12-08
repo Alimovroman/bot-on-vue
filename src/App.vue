e908be0b182b448c3859a89746024410728df81e
Roman <Alimov.ramon@gmail.com>
2023-12-08 03:07:50 +0300

    add mainPage

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <MainPage />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import MainPage from "./components/Main.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    MainPage,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Main.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="root">
+    <div><button v-on:click="onShowMessagePizza">пицца</button></div>
+    <div><button v-on:click="onShowMessageWeather">будильник</button></div>
+    <div><button v-on:click="onShowMessageClock">погода</button></div>
+
+    <div v-if="isShowMessagePizza">
+      Хорошо, я закажу пиццу, что еще мне сделать?
+    </div>
+    <div v-if="isShowMessageWeather">
+      Хорошо, погода отличная, что еще мне сделать?
+    </div>
+    <div v-if="isShowMessageClock">
+      Хорошо, будильник установлен, что еще мне сделать?
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isShowMessagePizza: false,
+      isShowMessageWeather: false,
+      isShowMessageClock: false,
+    };
+  },
+  methods: {
+    onShowMessagePizza() {
+      this.isShowMessagePizza = true;
+    },
+    onShowMessageWeather() {
+      this.isShowMessageWeather = true;
+    },
+    onShowMessageClock() {
+      this.isShowMessageClock = true;
+    },
+  },
+  name: "MainPage",
+};
+</script>
+
+<style>
+.root {
+  display: flex;
+}
+</style>
