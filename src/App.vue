158832d35322c97cf7ffde6ac878e495a06124d6
Roman <Alimov.ramon@gmail.com>
2023-12-08 21:27:50 +0300

    feat: chatBot completed

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
     <MainPage />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
 import MainPage from "./components/Main.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
     MainPage,
   },
 };
@@ -21,11 +17,14 @@ export default {
 
 <style>
 #app {
+  height: 100vh;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background: #7ca6cf;
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="root">
-    <div><button v-on:click="onShowMessagePizza">пицца</button></div>
-    <div><button v-on:click="onShowMessageWeather">будильник</button></div>
-    <div><button v-on:click="onShowMessageClock">погода</button></div>
-
-    <div v-if="isShowMessagePizza">
-      Хорошо, я закажу пиццу, что еще мне сделать?
+    <div v-if="isFirstLoading" class="loading">...</div>
+    <div v-else class="botMessage">Привет! Что я могу для вас сделать?</div>
+    <div
+      class="messagesWrapper"
+      v-for="message in botMessages"
+      :key="message.id"
+    >
+      <div class="botMessage">
+        {{ message }}
+      </div>
     </div>
-    <div v-if="isShowMessageWeather">
-      Хорошо, погода отличная, что еще мне сделать?
+    <div class="buttons" v-if="!isLoading && !isFirstLoading">
+      <div class="btnWrapper" v-if="!isShowMessagePizza">
+        <button v-on:click="onShowMessagePizza" class="btn">
+          Заказать пиццу
+        </button>
+      </div>
+      <div class="btnWrapper" v-if="!isShowMessageClock">
+        <button v-on:click="onShowMessageClock" class="btn">
+          Установить будильник
+        </button>
+      </div>
+      <div class="btnWrapper" v-if="!isShowMessageWeather">
+        <button v-on:click="onShowMessageWeather" class="btn">
+          Сказать какая погода
+        </button>
+      </div>
     </div>
-    <div v-if="isShowMessageClock">
-      Хорошо, будильник установлен, что еще мне сделать?
-    </div>
+    <div v-else-if="isLoading" class="loading">...</div>
   </div>
 </template>
 
@@ -20,19 +36,43 @@
 export default {
   data() {
     return {
+      botMessages: [],
       isShowMessagePizza: false,
       isShowMessageWeather: false,
       isShowMessageClock: false,
+      isLoading: false,
+      isFirstLoading: true,
     };
   },
+  mounted() {
+    setTimeout(() => {
+      this.isFirstLoading = false;
+    }, 1000);
+  },
   methods: {
+    addMessage(text) {
+      this.isLoading = true;
+      setTimeout(() => {
+        this.botMessages.push(
+          `Хорошо, ${text}, ${
+            this.botMessages.length === 2
+              ? "лимит вопросов исчерпан."
+              : "что еще мне сделать?"
+          }`
+        );
+        this.isLoading = false;
+      }, 1000);
+    },
     onShowMessagePizza() {
+      this.addMessage("я закажу пиццу");
       this.isShowMessagePizza = true;
     },
     onShowMessageWeather() {
+      this.addMessage("погода отличная");
       this.isShowMessageWeather = true;
     },
     onShowMessageClock() {
+      this.addMessage("будильник установлен");
       this.isShowMessageClock = true;
     },
   },
@@ -43,5 +83,61 @@ export default {
 <style>
 .root {
   display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  width: 400px;
+  height: 550px;
+  background: rgb(211, 236, 236);
+  border-radius: 20px;
+  padding-top: 20px;
+}
+.botMessage {
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  padding: 10px;
+  width: 110px;
+  border-radius: 10px;
+  border: #7290ac 1px solid;
+  align-self: flex-end;
+  margin: 10px;
+  background: rgb(201, 234, 234);
+}
+.messagesWrapper {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+}
+.buttons {
+  display: flex;
+}
+.btnWrapper {
+  display: flex;
+  justify-content: center;
+  background: #7290ac;
+  margin: 4px;
+  width: 110px;
+  border-radius: 10px;
+}
+.btn {
+  border: none;
+  background: inherit;
+  color: rgb(211, 236, 236);
+  padding: 10px;
+  border-radius: 10px;
+}
+.loading {
+  background: #7290ac;
+  color: rgb(211, 236, 236);
+  font-size: 22px;
+  padding: 10px;
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 30px;
+  align-self: self-start;
+  margin: 10px;
 }
 </style>
